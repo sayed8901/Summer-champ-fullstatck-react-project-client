86721b62a7261b9a1468bfe86bf32910e4004721
sayed8901 <sayed91515@gmail.com>
2023-08-13 23:29:39 +0600

    firebase confige modified

--- src/authProviders/firebase.config.js
+++ src/authProviders/firebase.config.js
@@ -5,22 +5,22 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID,
+  // apiKey: import.meta.env.VITE_APIKEY,
+  // authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  // projectId: import.meta.env.VITE_PROJECTID,
+  // storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  // messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  // appId: import.meta.env.VITE_APPID,
 
 
 
 
-//   apiKey: "AIzaSyBUJgVdkSoAbKIjM2rsRIQy-8Uv5ul6GV4",
-//   authDomain: "summer-champ-school-camping.firebaseapp.com",
-//   projectId: "summer-champ-school-camping",
-//   storageBucket: "summer-champ-school-camping.appspot.com",
-//   messagingSenderId: "490942082339",
-//   appId: "1:490942082339:web:ccfd1e23c0e76ceaba7096"
+  apiKey: "AIzaSyBUJgVdkSoAbKIjM2rsRIQy-8Uv5ul6GV4",
+  authDomain: "summer-champ-school-camping.firebaseapp.com",
+  projectId: "summer-champ-school-camping",
+  storageBucket: "summer-champ-school-camping.appspot.com",
+  messagingSenderId: "490942082339",
+  appId: "1:490942082339:web:ccfd1e23c0e76ceaba7096"
 };
 
 // Initialize Firebase
